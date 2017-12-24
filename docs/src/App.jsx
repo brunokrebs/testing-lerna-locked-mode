92d9ea1415787e4c65145462be9ac9fbae437040
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-24 09:01:36 -0200

    installing Auth0 React Components; adding a title;

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import Button from '../../packages/Button/src/Button';
-import Panel from '../../packages/Panel/src/Panel';
+import Button from 'bk1-react-button';
+import Panel from 'bk1-react-panel';
 import './App.css';
 
 const App = () => (
-  <Panel>
-    <p className="App-intro">
-      <Button text="I'm a simple button" onClick={() => console.log('button clicked')} />
-    </p>
-  </Panel>
+  [
+    <h1>Auth0 React Components</h1>,
+    <Panel>
+      <p className="App-intro">
+        <Button text="I'm a simple button" onClick={() => console.log('button clicked')} />
+      </p>
+    </Panel>,
+  ]
 );
 
 export default App;
