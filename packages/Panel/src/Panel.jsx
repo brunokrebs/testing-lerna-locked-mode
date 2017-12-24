d78ea2468efc8f0d569d8399103be13a5c2b2097
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-24 00:07:54 -0200

    adding a fake class to test release process;

--- packages/Panel/src/Panel.jsx
+++ packages/Panel/src/Panel.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import './Panel.css';
 
 const Panel = props => (
-  <div className="react-auth0">
+  <div className="react-auth0 shadow">
     {props.children}
   </div>
 );
