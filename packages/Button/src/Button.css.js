2ce5cf2e32a9ae12638be50c059656215a975238
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-24 16:06:26 -0200

    trying to test with Jest;

---
+++ packages/Button/src/Button.css.js
@@ -0,0 +1,10 @@
+export default {
+  border: '1px solid #EB5424',
+  'border-radius': '3px',
+  'background-color': '#EB5424',
+  color: '#fff',
+  padding: '5px 10px 5px 10px',
+  'margin-right': '10px',
+  cursor: 'pointer',
+  'line-height': '28px',
+};
