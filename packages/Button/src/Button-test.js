e619c6ffec02ce0d3334dbaeb924c4fa1c393922
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-25 10:57:39 -0200

    testing Button component;

--- packages/Button/src/Button-test.js
+++ packages/Button/src/Button-test.js
@@ -12,7 +12,22 @@ describe('Button', () => {
     assert(window.document.createElement, 'createElement is required to test React components');
     assert(ReactTestUtils, 'ReactTestUtils must be available');
     assert(renderIntoDocument, 'ReactTestUtils.renderIntoDocument must be available');
-    const element = renderIntoDocument(<Button onClick={() => (true)} text="Button Label" />);
-    assert(true, 'Button must be rendered');
+
+    let called = false;
+    function clickHandler() {
+      called = true;
+    }
+
+    const element = renderIntoDocument(
+      <div>
+        <Button onClick={clickHandler} text="Button Label" className="go-horse"/>
+      </div>,
+    );
+    assert(element, 'Button must be rendered');
+    const found = element.getElementsByClassName('go-horse')[0];
+    assert(found.tagName === 'BUTTON', 'must find');
+
+    ReactTestUtils.Simulate.click(found);
+    assert(called, 'must be clicked');
   });
 });
